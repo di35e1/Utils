cc0d7c334057ff11b628a1acb942a72dd1bc2226
Serge <di35e1@gmail.com>
2023-06-28 20:48:19 +0300

    Ddded advanced feature

--- CopyID.jsx
+++ CopyID.jsx
@@ -12,13 +12,16 @@ Last modifed 26/06/2023
 
 #target bridge
 
-const REGEX = /\d{3,}/g
+const REGEX = /\d{3,25}/
+const regexAgencies = /ria|RIA|AP|000_|i[sS]tock|[sS]putnik/
+const regexAFP = /(000_.{3,10}).jpg/
 
 if(BridgeTalk.appName == 'bridge'){
 
-    const bridgeVersion = Number((app.version.split('.'))[0])
+    const bridgeVer = Number((app.version.split('.'))[0])
+    const availableAPI = bridgeVer > 12
 
-    if(bridgeVersion > 12){
+    if(availableAPI){
         var copyResult = function(results){
             app.document.copyTextToClipboard(results) //Bridge 13 (2023) use API
         }
@@ -36,7 +39,7 @@ if(BridgeTalk.appName == 'bridge'){
         }
 
         //create new menu command in contextual
-        var copyCommandThumb = MenuElement.create('command', '• Copy ID numbers', '-after Thumbnail/Open-')
+        var copyCommandThumb = MenuElement.create('command', '• Copy ID numbers', '-after Thumbnail/Open')
         copyCommandThumb.onSelect = function(){
             numbersToClipboard()
             }
@@ -45,24 +48,103 @@ if(BridgeTalk.appName == 'bridge'){
         alert(e + ' ' + e.line)
     }
 
+    try{
+        //create new menu command in Tools
+        var extendedCopyCommand = MenuElement.create('command', '• Copy Source and ID...', 'at the end of Tools')
+        extendedCopyCommand.onSelect = function(){
+            sourseAndIdToClipboard()
+        }
+
+        //create new menu command in contextual
+        var extendedCopyCommandThumb = MenuElement.create('command', '• Copy Source and ID...', 'after Thumbnail/Open-')
+        extendedCopyCommandThumb.onSelect = function(){
+            sourseAndIdToClipboard()
+            }
+    }
+    catch(e){
+        alert(e + ' ' + e.line)
+    }
+
     function numbersToClipboard(){
-        var fileList = []
         var idList = []
+        var idNumber
+        var idAFP
+        var cnfMessage
 
         try{
             var thumbs = app.document.selections
             for (var key in thumbs){
-                fileList.push(thumbs[key].name)
+                idNumber = (thumbs[key].name).match(REGEX)
+                if (idNumber == '000'){ // forexample 000_******.jpg
+                    idAFP = idNumber.input.match(regexAFP)
+                    if (idAFP) idList.push(idAFP[1].replace(/["]/g, '\\"'))
+                } else {
+                    if (idNumber) idList.push(idNumber.toString())
+                }
             }
 
-            idList = fileList.toString().match(REGEX)
-
-            if (idList == null){
-                Window.alert('There are no numbers')
+            if (idList.length == 0){
+                Window.alert('There are no numbers to copy')
             } else {
-                var cnfMessage = 'Would you like to get ' + idList.length + ' numbers?\n' + idList.join(", ")
+                cnfMessage = 'Would you like to get ' + idList.length + ' numbers?\n' + idList.join(", ")
                 if (Window.confirm(cnfMessage)){
                     copyResult(idList.join(" "))
+                }
+            }
+            return
+        } 
+        catch(e) {
+            alert(e + ' ' + e.line)
+        }
+    }
+
+    function sourseAndIdToClipboard(){
+        var itemList_ = [] 
+        var itemIndex = 0
+        var itemId
+        var item
+
+        try{
+            var thumbs_ = app.document.selections
+
+            for (var key in thumbs_){
+                itemIndex++
+                item = thumbs_[key].name
+                agency = String(item.match(regexAgencies)).toLowerCase()
+
+                if (agency == '000_'){
+                    itemId = (item.match(regexAFP))
+                    if (itemId) itemList_.push(itemIndex + '. AFP:  ' + itemId[1].replace(/["]/g, '\\"'))
+
+                } else if (agency == 'ap'){
+                    itemId = item.match(REGEX)
+                    if (itemId) itemList_.push(itemIndex + '. AP:  ' + itemId)
+
+                } else if (agency == "ria" || agency == 'sputnik'){
+                    itemId = item.match(REGEX)
+                    if (itemId) itemList_.push(itemIndex + '. RIA:  ' + itemId)
+
+                } else if (agency == "istock"){
+                    itemId = item.match(REGEX)
+                    if (itemId) itemList_.push(itemIndex + '. iStock:  ' + itemId)
+
+                } else {
+                    itemId = item.replace(/["]/g, '\\"') //Quotedform
+                    if (itemId) itemList_.push(itemIndex + '. Unknown:  ' + itemId)
+                }
+            }
+
+            if (itemList_.length == 0){
+                Window.alert('There are no items to copy', 'Copy Source and ID...')
+            } else if (itemList_.length < 26) {
+                var cnfMessage_ = 'Would you like to get ' + itemList_.length + ' items?\n' + itemList_.join("\n")
+                if (Window.confirm(cnfMessage_, false, 'Copy Source and ID...')){
+                    copyResult(itemList_.join("\n"))
+                }
+            } else {
+                var cnfMessage_ = 'Would you like to get ' + itemList_.length + ' items?\n' + itemList_.slice(0,22).join("\n") + '\n...\n' + itemList_.slice(-1)
+                if (Window.confirm(cnfMessage_, false, 'Copy Source and ID...')){
+                    copyResult(itemList_.join("\n"))
                 }
             }
             return
